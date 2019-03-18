9dda7c42e05960537134b39d99e31a225f3981b0
jire4plus <yiresse.abia@gmail.com>
2019-03-18 15:24:53 +0100

    npm installation for type ormm done

--- lib/app.ts
+++ lib/app.ts
@@ -3,6 +3,7 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import { Routes } from "./routes/crmRoutes";
 import * as mongoose from "mongoose";
+import "reflect-metadata";
 
 
 class App {
