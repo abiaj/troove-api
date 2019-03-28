32ad2a8642a871a4e2c333a1527525d4d6d45d29
jire4plus <yiresse.abia@gmail.com>
2019-03-28 17:01:53 +0100

    update cuser controller with login

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -4,9 +4,62 @@ import { Request, Response } from 'express';
 import { UserRepo } from "../repository/User";
 import  { User } from "../entity/User";
 
+//nestjs
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
+
+
 const UserModel = mongoose.model('User', UserSchema);
 
 export class UserController{
+
+   private saltRounds = 10;
+
+   constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>) {
+
+    }
+
+    async getUsers(): Promise<User[]> {
+    return await this.userRepository.find();
+    }
+    async getUserByUsername(username: string): Promise<User> {
+    return (await this.userRepository.find({ username }))[0];
+    }
+
+    async createUser(user: User): Promise<User> {
+      let userRepo: UserRepo = new UserRepo();
+      user.passwordHash = await this.getHash(user.password);
+      // clear password as we don't persist passwords
+      user.password = undefined;
+
+      console.log(req.body);
+      let user:User = new User();
+      user.name = req.body.name;
+      user.username = req.body.username;
+      user.email = req.body.email;
+      //user.password = req.body.password;
+      user.phoneNumber = req.body.mobileprefix+req.body.phoneNumber;
+      user.gender = req.body.gender;
+      user.userType = req.body.userType;
+      userRepo.createUser(user).then((result: any) => {
+          console.log("Result : " + result);
+          res.status(200).send({
+              data: result,
+              message: 'User created successfully'
+          })
+      });
+    }
+
+    async getHash(password: string|undefined): Promise<string> {
+        return bcrypt.hash(password, this.saltRounds);
+    }
+
+    async compareHash(password: string|undefined, hash: string|undefined): Promise<boolean> {
+      return bcrypt.compare(password, hash);
+    }
 
     //Using mongo
     public signup (req: Request, res: Response) {
@@ -41,28 +94,4 @@ export class UserController{
             })
         });
     }
-
-    public createUser (req: Request, res: Response) {
-        let userRepo: UserRepo = new UserRepo();
-
-        console.log("Received SaveEmployee ==> POST");
-        console.log(req.body);
-
-        let user:User = new User();
-        user.name = req.body.name;
-        user.username = req.body.username;
-        user.email = req.body.email;
-        user.password = req.body.password;
-        user.phoneNumber = req.body.mobileprefix+req.body.phoneNumber;
-        user.gender = req.body.gender;
-        user.userType = req.body.userType;
-        userRepo.createUser(user).then((result: any) => {
-            console.log("Result : " + result);
-            res.status(200).send({
-                data: result,
-                message: 'User created successfully'
-            })
-        });
-    }
-
 }
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,12 +1,13 @@
-import {Entity, Column, PrimaryGeneratedColumn} from "typeorm";
+import {Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn} from "typeorm";
 
 enum Gender {
   Male = "male",
   Female = "female"
 }
 enum UserType {
-  User = "user",
-  Businness = "businness"
+  User ,
+  Businness ,
+  Admin
 }
 
 @Entity("users")
@@ -24,8 +25,11 @@ export class User {
     @Column({ type: "varchar",length:200, unique: true , nullable: false })
     email: string;
 
-    @Column( "text")
-    password: string;
+    @Column({ length: 100, nullable: true })
+    password: string|undefined;
+
+    @Column({ length: 100, nullable: true })
+    passwordHash: string|undefined;
 
     @Column({ nullable: true })
     phoneNumber: string;
@@ -44,6 +48,5 @@ export class User {
 
     @UpdateDateColumn()
     updatedDate: Date;
-
 
 }
